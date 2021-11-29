81214107d4ac5f15efbeceeb5f9c1982cd011438
kevinjn0123 <kevinjn0123@users.noreply.github.com>
2021-11-29 18:37:41 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Lumki\Lumki\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Lumki\Lumki\LumkiServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
