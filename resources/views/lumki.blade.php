c8227bc185ea4f1817018dffc8bd7ac58636f69a
Kevinjn0123 <kevinjn0123@gmail.com>
2021-11-30 19:47:04 +0530

    Initial commit

--- resources/views/lumki.blade.php
+++ resources/views/lumki.blade.php
@@ -17,11 +17,11 @@
 
     <!-- Scripts -->
     @routes
-    <script src="{{ mix('js/app.js') }}" defer></script>
-    <script src="{{ mix('js/vendor.js') }}" defer></script>
-    <script src="{{ mix('js/manifest.js') }}" defer></script>
 </head>
 <body class="font-sans antialiased">
 @inertia
+<script>
+    {!! file_get_contents($jsPath) !!}
+</script>
 </body>
 </html>
