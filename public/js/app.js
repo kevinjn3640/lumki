37ada1fc808d3cae164eca3b89e1ca908be98f05
Kevinjn0123 <kevinjn0123@gmail.com>
2021-11-30 19:59:38 +0530

    Initial commit

--- public/js/app.js
+++ public/js/app.js
@@ -38,6 +38,22 @@ exports["default"] = User;
 "use strict";
 
 
+var __assign = this && this.__assign || function () {
+  __assign = Object.assign || function (t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+
+      for (var p in s) {
+        if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+      }
+    }
+
+    return t;
+  };
+
+  return __assign.apply(this, arguments);
+};
+
 var __importDefault = this && this.__importDefault || function (mod) {
   return mod && mod.__esModule ? mod : {
     "default": mod
@@ -73,11 +89,11 @@ var appName = ((_a = window.document.getElementsByTagName('title')[0]) === null 
 
     return page;
   },
-  setup: function setup(_ref) {
-    var el = _ref.el,
-        App = _ref.App,
-        props = _ref.props;
-    return (0, react_dom_1.render)(react_1["default"].createElement(App, Object.assign({}, props)), el);
+  setup: function setup(_a) {
+    var el = _a.el,
+        App = _a.App,
+        props = _a.props;
+    return (0, react_dom_1.render)(react_1["default"].createElement(App, __assign({}, props)), el);
   }
 });
 progress_1.InertiaProgress.init({
