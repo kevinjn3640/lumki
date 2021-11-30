d79f07805facdbe90460af37e996b34f7713eada
Kevinjn0123 <kevinjn0123@gmail.com>
2021-11-30 18:17:50 +0530

    Initial commit

--- resources/js/Pages/User.tsx
+++ resources/js/Pages/User.tsx
@@ -1,15 +1,3 @@
-// <template>
-//     <div>{{ users.email }}</div>
-// </template>
-//
-// <script>
-// export default {
-//     props: ['users'],
-//     mounted() {
-//         console.log('Example component mounted.')
-//     }
-// }
-// </script>
 import React from "react";
 
 const User = () => {
