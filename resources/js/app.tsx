aecb56cff68aeb8bb8f41dc0b2a3e61e3ceb283a
Kevinjn0123 <kevinjn0123@gmail.com>
2021-11-30 18:27:20 +0530

    Initial commit

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,25 +1,10 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {createInertiaApp} from '@inertiajs/inertia-react';
-import {InertiaProgress} from '@inertiajs/progress';
-
-const appName =
-    window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+import React from 'react'
+import { render } from 'react-dom'
+import { createInertiaApp } from '@inertiajs/inertia-react'
 
 createInertiaApp({
-    title: title => `${title} - ${appName}`,
-    resolve: name => {
-        const page = require(`./Pages/${name}`);
-        // if (page.layout === undefined && !name.startsWith('Public/')) {
-        //   page.layout ??= Layout;
-        // }
-        return page;
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props }) {
+        render(<App {...props} />, el)
     },
-    setup({el, App, props}) {
-        return render(
-            <App {...props} />, el,
-        );
-    },
-});
-
-InertiaProgress.init({color: '#4B5563'});
+})
