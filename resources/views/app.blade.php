38005477c82595298422e2830d3e4003cae9755a
Kevinjn0123 <kevinjn0123@gmail.com>
2021-11-30 18:16:18 +0530

    Initial commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,33 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-    <!-- Scripts -->
     <!-- Styles -->
-{{--    <style>--}}
-{{--        {!! file_get_contents($cssPath) !!}--}}
-{{--    </style>--}}
-<!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     <!-- Scripts -->
     @routes
     <script src="{{ mix('js/app.js') }}" defer></script>
-
+    <script src="{{ mix('js/vendor.js') }}" defer></script>
+    <script src="{{ mix('js/manifest.js') }}" defer></script>
 </head>
 <body class="font-sans antialiased">
 @inertia
-
-<!-- Scripts -->
-{{--<script>--}}
-{{--    {!! file_get_contents($jsPath) !!}--}}
-{{--</script>--}}
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.ts('resources/js/app.tsx', 'public/js/app.js')
+    .extract()
     .react()
     .postCss('resources/css/app.css', 'public/css/app.css', [
         require('postcss-import'),
