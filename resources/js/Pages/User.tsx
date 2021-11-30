163aa5607ded152530d23b856d449952f4f96928
Kevinjn0123 <kevinjn0123@gmail.com>
2021-11-30 18:05:00 +0530

    Initial commit

---
+++ resources/js/Pages/User.tsx
@@ -0,0 +1,23 @@
+// <template>
+//     <div>{{ users.email }}</div>
+// </template>
+//
+// <script>
+// export default {
+//     props: ['users'],
+//     mounted() {
+//         console.log('Example component mounted.')
+//     }
+// }
+// </script>
+import React from "react";
+
+const User = () => {
+    return (
+        <>
+            <div>Hi</div>
+        </>
+    )
+}
+
+export default User;
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js/app.js').vue()
+mix.ts('resources/js/app.tsx', 'public/js/app.js')
+    .react()
     .postCss('resources/css/app.css', 'public/css/app.css', [
         require('postcss-import'),
         require('tailwindcss'),
