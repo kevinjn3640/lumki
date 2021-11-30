b7024da2e307fdc2b41f1551ee897c42022f2a46
Kevinjn0123 <kevinjn0123@gmail.com>
2021-11-30 21:45:43 +0530

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Lumki\Lumki\Http\Controllers\PostController;
 
-Route::get('/posts', PostController::class)->name('posts.index');
-//Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-//Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
--- src/Http/Controllers/PostController.php
+++ src/Http/Controllers/PostController.php
@@ -2,46 +2,58 @@
 
 namespace Lumki\Lumki\Http\Controllers;
 
-use Illuminate\Support\Facades\View;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\View;
+use App\Models\User;
 
 class PostController
 {
-
-    /**
-     * Show the billing portal.
-     *
-     * @param  string|null  $type
-     * @param  mixed  $id
-     * @return \Inertia\Response
-     */
-    public function __invoke($type = null, $id = null)
+    public function index()
     {
-
+//        dd("HI");
         Inertia::setRootView('lumki::lumki');
 
         View::share([
             'cssPath' => __DIR__.'/../../../public/css/app.css',
             'jsPath' => __DIR__.'/../../../public/js/app.js',
+//            'manifestPath' => __DIR__.'/../../../public/js/manifest.js',
+//            'vendorPath' => __DIR__.'/../../../public/js/vendor.js',
 //            'translations' => static::getTranslations(),
         ]);
 
 //        Inertia::share(app(FrontendState::class)->current($type, $billable));
 
-        return Inertia::render('User');
+        return Inertia::render('User', [
+            'users' => User::find(1),
+        ]);
     }
-//
-//    /**
-//     * Get the Spark translations from the appropriate language file.
-//     *
-//     * @return string
-//     */
-//    private static function getTranslations()
-//    {
-//        if (! is_readable($file = resource_path('lang/spark/'.app()->getLocale().'.json'))) {
-//            $file = resource_path('lang/spark/'.app('translator')->getFallback().'.json');
+
+    public function show()
+    {
+        //
+    }
+
+    public function store()
+    {
+        // Let's assume we need to be authenticated
+        // to create a new post
+//        if (! auth()->check()) {
+//            abort (403, 'Only authenticated users can create new posts.');
 //        }
 //
-//        return is_readable($file) ? file_get_contents($file) : '{}';
-//    }
+//        request()->validate([
+//            'title' => 'required',
+//            'body'  => 'required',
+//        ]);
+//
+//        // Assume the authenticated user is the post's author
+//        $author = auth()->user();
+//
+//        $post = $author->posts()->create([
+//            'title'     => request('title'),
+//            'body'      => request('body'),
+//        ]);
+//
+//        return redirect(route('posts.show', $post));
+    }
 }
