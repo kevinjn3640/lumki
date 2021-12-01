9e880c78e49b8d4374aa279ae71ce7b7b3f27cde
Kevinjn0123 <kevinjn0123@gmail.com>
2021-12-01 16:38:57 +0530

    Initial commit

--- config/lumki.php
+++ config/lumki.php
@@ -1,5 +1,6 @@
 <?php
 // config for Lumki/Lumki
 return [
-    'prefix' => 'lumki'
+    'prefix' => 'lumki',
+    'middleware' => [\Laravel\Jetstream\Http\Middleware\ShareInertiaData::class, 'web']
 ];
